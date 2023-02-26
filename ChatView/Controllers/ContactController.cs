0c4da26aa4e4d88c41458a3a94f849dd0749aa71
nokspec <63341284+nokspec@users.noreply.github.com>
2023-02-26 20:34:16 +0100

    Add project files.

---
+++ ChatView/Controllers/ContactController.cs
@@ -0,0 +1,39 @@
+﻿using ChatView.Models;
+using ChatView.Models.Contact;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ChatView.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+
+        [HttpPost]
+        public IActionResult Create(Contact contactForm)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Index");
+            }
+            return View("Index");
+        }
+
+        public ContactController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ChatView/Models/Contact/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatView.Models.Contact
+{
+    public class Contact
+    {
+        [Required(ErrorMessage = "Please enter a topic")]
+        public string Topic { get; set; }
+
+        [Required(ErrorMessage = "Please enter your e-mail address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your message")]
+        public string Message { get; set; }
+
+    }
+}
