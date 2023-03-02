6299cb13b0b4dcb7d1a39a62bc774787378652e4
nokspec <63341284+nokspec@users.noreply.github.com>
2023-03-02 19:08:23 +0100

    Profile & ChatView styling

---
+++ ChatView/Controllers/ChatViewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatView.Controllers
+{
+    public class ChatViewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
