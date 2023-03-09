efae71e7e0d214661ffcbfbe3f757ee4fd3b1a40
nokspec <63341284+nokspec@users.noreply.github.com>
2023-03-09 14:18:43 +0100

    ChatView URL box added

--- ChatView/Controllers/ChatViewController.cs
+++ ChatView/Controllers/ChatViewController.cs
@@ -1,12 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ChatView.Models.ChatView;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ChatView.Controllers
 {
     public class ChatViewController : Controller
     {
+        private readonly HttpClient _httpClient;
+
+        public ChatViewController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DownloadVideo(string url)
+        {
+            if (url != null)
+            {
+                if (IsValidURL(url))
+                {
+                    var apiUrl = "http://localhost:5134/api/chatview/newvideo";
+                    var payload = new NewVideo
+                    {
+                        Url = url
+                    };
+
+                    var content = new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json");
+                    var response = await _httpClient.PostAsync(apiUrl, content);
+
+                    if (response.IsSuccessStatusCode) return View("Index");
+                    return View("Index");
+                }
+            }
+            return View("Index");
+        }
+
+        private static bool IsValidURL(string url)
+        {
+            string Pattern = @"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$";
+            Regex Rgx = new(Pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            return Rgx.IsMatch(url);
         }
     }
 }
---
+++ ChatView/Models/ChatView/NewVideo.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatView.Models.ChatView
+{
+    public class NewVideo
+    {
+        public string Url { get; set; }
+        //public User User { get; set; } later uitbreiden (kijk Keep)
+    }
+}
