45441ed07b672d70e48492fa59d53134fe596548
nokspec <63341284+nokspec@users.noreply.github.com>
2023-06-05 17:34:09 +0200

    fix?

--- ChatView_API/Controllers/ChatViewController.cs
+++ ChatView_API/Controllers/ChatViewController.cs
@@ -24,7 +24,6 @@ namespace ChatView_API.Controllers
         [HttpPost]
         public async Task<IActionResult> DownloadNewVideo([FromBody] NewVideo newVideo)
         {
-            Console.WriteLine("Nu in de API");
             if (string.IsNullOrEmpty(newVideo?.Url) || !_youTubeUrlRegex.IsMatch(newVideo.Url))
             {
                 return BadRequest("Invalid URL");
